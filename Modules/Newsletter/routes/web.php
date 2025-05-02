0539aeadf51c1e9ab29c4ce2eacd17877fd0f5c2
Valentaizar Hitsukaya <valentaizarstone@ymail.com>
2025-05-03 02:51:40 +0300

    Update Modules\Newsletter routes\web.php Unsubscribe

--- Modules/Newsletter/routes/web.php
+++ Modules/Newsletter/routes/web.php
@@ -9,16 +9,16 @@ use Modules\Newsletter\Http\Livewire\NewsletterUnsubscribe;
 Route::get('/newsletter', NewsletterSubscription::class)->name('newsletter');
 Route::get('/send-newsletter', [NewsletterController::class, 'sendNewsletter']);
 
-Route::get('newsletter/unsubscribe/{email}', function ($email) {
-    $subscriber = Subscription::where('email', $email)->first();
+// Route::get('newsletter/unsubscribe/{email}', function ($email) {
+//     $subscriber = Subscription::where('email', $email)->first();
 
-    if ($subscriber) {
-        $subscriber->delete();
-        return response()->json(['message' => 'You have been unsubscribed successfully.']);
-    }
+//     if ($subscriber) {
+//         $subscriber->delete();
+//         return response()->json(['message' => 'You have been unsubscribed successfully.']);
+//     }
 
-    return response()->json(['message' => 'Subscriber not found.'], 404);
-})->name('newsletter.unsubscribe');
+//     return response()->json(['message' => 'Subscriber not found.'], 404);
+// })->name('newsletter.unsubscribe');
 
 Route::get('/unsubscribe/{email}/{token}', [NewsletterUnsubscribe::class, 'unsubscribe'])->name('newsletter.unsubscribe');
 
